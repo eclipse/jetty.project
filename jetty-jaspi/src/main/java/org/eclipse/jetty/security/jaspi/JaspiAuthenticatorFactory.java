0e89fff249599e7167469e0ab60c3f144b491c3c
Frode Carlsen <frode.odde.carlsen@nav.no>
2021-07-19 18:29:04 +0200

    codereview: using ServletContext#getVirtualServerName in matching logic for finding correct authenticator config. Updated comments/doc

--- jetty-jaspi/src/main/java/org/eclipse/jetty/security/jaspi/JaspiAuthenticatorFactory.java
+++ jetty-jaspi/src/main/java/org/eclipse/jetty/security/jaspi/JaspiAuthenticatorFactory.java
@@ -104,7 +104,7 @@ public class JaspiAuthenticatorFactory extends DefaultAuthenticatorFactory
             };
 
             Subject serviceSubject = findServiceSubject(server);
-            String serverName = findServerName(server);
+            String serverName = findServerName(context, server);
 
             String contextPath = context.getContextPath();
             if (contextPath == null || contextPath.length() == 0)
@@ -158,17 +158,23 @@ public class JaspiAuthenticatorFactory extends DefaultAuthenticatorFactory
 
     /**
      * Find a servername. If {@link #setServerName(String)} has not been called,
-     * then use the name of the a principal in the service subject. If not found,
-     * return "server".
+     * then use the virtualServerName of the context. 
+     * If this is also null, then use the name of the a principal in the service subject. 
+     * If none are found, return "server".
+     * @param context 
      *
      * @param server the server to find the name of
      * @return the server name from the service Subject (or default value if not
      *         found in subject or principals)
      */
-    protected String findServerName(Server server)
-    {
+    protected String findServerName(ServletContext context, Server server)
+    {   
         if (_serverName != null)
             return _serverName;
+        
+        String virtualServerName = context.getVirtualServerName();
+        if(virtualServerName != null)
+            return virtualServerName;
 
         Subject subject = findServiceSubject(server);
         if (subject != null)
