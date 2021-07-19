a0cf40b2e07b2ebeeea242d08e6dc943cd614b21
Frode Carlsen <frode.odde.carlsen@nav.no>
2021-07-19 18:30:06 +0200

    checkstyle

--- jetty-jaspi/src/main/java/org/eclipse/jetty/security/jaspi/JaspiAuthenticatorFactory.java
+++ jetty-jaspi/src/main/java/org/eclipse/jetty/security/jaspi/JaspiAuthenticatorFactory.java
@@ -173,7 +173,7 @@ public class JaspiAuthenticatorFactory extends DefaultAuthenticatorFactory
             return _serverName;
         
         String virtualServerName = context.getVirtualServerName();
-        if(virtualServerName != null)
+        if (virtualServerName != null)
             return virtualServerName;
 
         Subject subject = findServiceSubject(server);
